6ce28c5a7604b892ef045a9e769ed42cf8a636b2
Haoyang Liu <haoyang.liu@uqconnect.edu.au>
2020-04-15 17:20:51 +1000

    valid parentheses

--- valid_parentheses.js
+++ valid_parentheses.js
@@ -2,6 +2,25 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
+s = "[{()}]";
 
+var isValid = function(s) {
+    let map = {
+        ")": "(",
+        "]": "[",
+        "}": "{",
+    };
+    let arr = [];
+    for (i = 0; i < s.length; i++) {
+        if (s[i] === "(" || s[i] === "[" || s[i] === "{") {
+            arr.push(s[i]);
+        } else {
+            if (arr[arr.length - 1] === map[s[i]]) {
+                arr.pop();
+            } else return false;
+        }
+    }
+    return arr.length === 0 ? true : false;
 };
+
+isValid(s)
